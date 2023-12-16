04e87b4cbc2e14f2e4ad101593a5b713a92f7e28
Primum1 <primum_mobile@mail.ru>
2023-12-17 00:09:13 +0500

    На 16 строке  пишет ошибку,поправьте меня, что не так?

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello ");
     }
-}
+    int[] arr = generateRandomArray();
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    //Задание 1
+    int sum = 0;
+       for (int b1 : arr) {
+        sum = sum + b1;
+    }
+    System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+    }
+
+
